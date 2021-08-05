ed753480b5f575928ef768ba854755a266af8e40
BrayanPr <basecoco1@gmail.com>
2021-08-04 20:26:23 -0600

    creating rides

--- rides/views.py
+++ rides/views.py
@@ -30,5 +30,4 @@ class createRideViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins
         serializer = RideSerializer(data=data)
         serializer.is_valid()
         headers = self.get_success_headers(serializer.data)
-
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
